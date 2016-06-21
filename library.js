287e3f8f128af6e33e03d4a28e9c3b4e7d6aaabd
annemijn <annemijn.portier@hotmail.com>
2016-06-21 19:24:33 +0200

    Update library.js

--- library.js
+++ library.js
@@ -11,6 +11,15 @@ var username = document.myform.username.value; //kijk wat de value is van de geb
 		bericht += message;
 		document.myform.username.style.borderColor = 'red'; //maak een rood randje 
 	}
+		else
+		{
+			var pattUsername = /^[a-z0-9_-]{3,15}$/; //3-15 karakters met alleen letters, cijfer of symbool“_-”.
+			if(!pattUsername.test(username))
+			{
+				message += '- De gebruikersnaam moet tussen de 3-15 karakters zijn met alleen de symbolen _-<br>';
+				bericht += message;
+			}
+		}
 	
 	var password = document.myform.password.value; //kijk wat de value is van het wachtwoord
 	if (password == '') //als het leeg is
